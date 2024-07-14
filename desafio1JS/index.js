182c04b449416414d0bbdfb2ddf0e5bea080152e
CristianFFFweb <cristian.perez.loezar@gmail.com>
2024-07-14 16:31:49 -0400

    1er desafio js

---
+++ desafio1JS/index.js
@@ -0,0 +1,12 @@
+const precio = 400000;
+let suma=0;
+let resta=0;
+
+
+let cantidadSuma=document.querySelector(".cantidad");
+
+
+
+let precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+let totalSpan = document.querySelector(".valor-total");
